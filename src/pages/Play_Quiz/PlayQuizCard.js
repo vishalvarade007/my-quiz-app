0b7338e3bbfa79ae9f649e86fe54b1f4aaf1b719
varadevishal07@gmail.com <varadevishal07@gmail.com>
2024-02-25 10:54:36 +0530

    sixth commit

--- src/pages/Play_Quiz/PlayQuizCard.js
+++ src/pages/Play_Quiz/PlayQuizCard.js
@@ -1,7 +1,95 @@
+import { useState } from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {getAnswer} from "../../Redux/Actions/Actions";
+import { Button, Container } from "@mui/material";
+import { Result } from "./Result";
+import "./PlayQuizCardStyle.css";
+
 export let PlayQuizCard = ()=>{
+    const [count,setCount] = useState(0);
+    const [showModal,setShowmodal] = useState(false);
+    const [finalAnswer,setFinalanswer] = useState({});
+    const [disable,setDisable] = useState(true);
+    const quiz = useSelector((state)=>state.reducer.playQuiz).question;
+    const title = useSelector((state)=>state.reducer.title);
+
+    const name = useSelector((state)=> state.reducer.name);
+    const dispatch = useDispatch();
+    const question = quiz[count].question;
+    const answers = quiz[count].answers;
+
+    const nextQuestionHandler = ()=>{
+        dispatch(getAnswer(finalAnswer));
+        setDisable(true);
+        if(count >= quiz.length - 1){
+            setShowmodal(true);
+            setCount((prev)=>prev);
+        } else {
+            setCount((prev)=>prev + 1);
+        }
+    };
+
+    const onclickHandler = (el) => {
+        getAnswerHandler(el.answer, el.correct, el.id);
+        setDisable(false);
+    };
+
+    const getAnswerHandler = (answer, correct, id) => {
+        const Answer = {
+          answer: answer,
+          isCorrect: correct,
+          id: id,
+        };
+        setFinalanswer(Answer);
+    };
+
     return (
         <>
-           play quiz card
+          <Container>
+            <div className="contain">
+                <div>
+                    {showModal ? (<Result name={name}></Result>
+                    ):(
+                        <div className="quizcard-container">
+                            <div className="quizcard-title">
+                                <h1>{title}</h1>
+                            </div>
+                            <h2>
+                                Q.{count + 1} {question}
+                            </h2>
+                            <div>
+                                {answers.map((el,i)=>(
+                                    <div className="quizcard-option" onClick={()=>onclickHandler(el)}
+                                     style={{background:`${finalAnswer.id === el.id ? "green":"cornsilk"}`,}}
+                                     key={i}
+                                    >
+                                     <p>{el.answer}</p>
+                                    </div>
+                                ))}
+                            </div>
+                            <div>
+                                <div>
+                                    <h3>
+                                        Question {count + 1}/{quiz.length}
+                                    </h3>
+                                </div>
+                                <div className="next-question">
+                                    {disable ? (
+                                        <Button variant="outlined" color="error" sx={{":hover":{bgcolor:"rgb(206,78,78)", color:"white"}}}>
+                                            Next Question
+                                        </Button>
+                                    ):(
+                                        <Button variant="outlined" color="error" sx={{":hover":{bgcolor:"rgb(206,78,78)", color:"white"}}} onClick={nextQuestionHandler}>
+                                            Next Question
+                                        </Button>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+          </Container>
         </>
     )
 }
--- src/pages/Play_Quiz/Result.js
+++ src/pages/Play_Quiz/Result.js
@@ -1,7 +1,55 @@
-export let Result = ()=>{
+import React,{ useEffect, useState } from "react";
+import {useDispatch, useSelector} from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {resetQuiz} from "../../Redux/Actions/Actions";
+import { Button } from "@mui/material";
+import "./ResultStyle.css";
+
+
+
+export let Result = (props)=>{
+    const [isAnimating,setIsAnimating] = useState(false);
+    useEffect(()=>{
+       setIsAnimating(true);
+       const animationTimeout = setTimeout(()=>{
+          setIsAnimating(false);
+       },2000);
+       return ()=>{
+        clearTimeout(animationTimeout);
+       }
+    },[]);
+
+    const results = useSelector((state)=>state.reducer.answers);
+    const mapped = results.map((el)=>el.isCorrect);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const resetQuizHandler = ()=>{
+        dispatch(resetQuiz());
+        navigate("/");
+    }
+    console.log("props-name:",props);
     return (
         <>
-         Result 
+         <div className="result-container">
+            <div className="container">
+                <div className="result">
+                    <p className="name">Heyy !! {props.name}</p>
+                    <div className={(mapped.filter((el)=>el === true).length !== 0)?"result-img" : "result-img2"}></div>
+                    {(mapped.filter((el)=>el === true).length !== 0) ? (<div className={`congo ${isAnimating ? "animate" : ""}`}>
+                        <h2>Congratulations !</h2>
+                    </div>) : (<div className={`congo ${isAnimating ? "animate" : ""}`}>
+                        <h2 style={{fontSize:"30px"}}>Oops!</h2>
+                    </div>)}
+                    <p className="score" style={{marginBottom:"15px",marginTop:"0px"}}>
+                        You've scored {mapped.filter((el)=>el === true).length} out of {" "} {mapped.length}
+                    </p>
+                    <Button variant="outlined" color="error" sx={{":hover":{bgcolor:"rgb(206,78,78)",color:"white"},marginBottom:"20px"}} onClick={()=> resetQuizHandler()}>
+                        Done
+                    </Button>
+                </div>
+            </div>
+         </div>
         </>
     )
 }
